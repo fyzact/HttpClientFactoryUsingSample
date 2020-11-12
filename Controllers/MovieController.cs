87c0acea8ceb5f52a17c98c27b7ede3638d81c83
fyzact <feyyazacet@gmail.com>
2020-11-12 22:42:21 +0300

    implement secont way using IHttpClientFactory

---
+++ Controllers/MovieController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using HttpClientFactoryUsingSample.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HttpClientFactoryUsingSample.Controllers
+{
+    [Route("api/movies")]
+    [ApiController]
+    public class MovieController : BaseController
+    {
+        HttpClient _httpClient;
+        public MovieController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClient = httpClientFactory.CreateClient("movie-sample-api");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var movieJson = await _httpClient.GetStringAsync("movies/api/animation");
+            var movies = Deserialize<IEnumerable<Movie>>(movieJson);
+            return Ok(movies);
+        }
+    }
+}
---
+++ Models/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HttpClientFactoryUsingSample.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string PosterURL { get; set; }
+        public string ImdbId { get; set; }
+
+    }
+}
